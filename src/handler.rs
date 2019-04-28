6df960d8d6a0f79a67e1fb2d3dd3d2ccf2d455d9
tensor-programming <abeltensor@tensor-programming.com>
2019-04-28 16:42:26 -0400

    final commit

--- src/handler.rs
+++ src/handler.rs
@@ -4,3 +4,38 @@ use crate::model::AppState;
 use actix_web::{
     error, ws, AsyncResponder, FutureResponse, HttpRequest, HttpResponse, Json, State,
 };
+use futures::Future;
+use uuid::Uuid;
+
+pub fn login((user, state): (Json<User>, State<AppState>)) -> FutureResponse<HttpResponse> {
+    state
+        .db
+        .send(user.into_inner())
+        .from_err()
+        .and_then(|response| match response {
+            Ok(msgs) => Ok(HttpResponse::Ok().json(msgs)),
+            Err(_) => Ok(HttpResponse::InternalServerError().json("Internal Server Error")),
+        })
+        .responder()
+}
+
+pub fn send_msg((form, state): (Json<Form>, State<AppState>)) -> FutureResponse<HttpResponse> {
+    let ws = state.ws.clone();
+
+    state
+        .db
+        .send(form.into_inner())
+        .from_err()
+        .and_then(move |response| match response {
+            Ok(result) => {
+                ws.do_send(result.clone());
+                Ok(HttpResponse::Ok().json(result.clone()))
+            }
+            Err(_) => Ok(HttpResponse::InternalServerError().json("Internal Server Error")),
+        })
+        .responder()
+}
+
+pub fn get_ws(req: &HttpRequest<AppState>) -> Result<HttpResponse, error::Error> {
+    ws::start(req, Session(Uuid::nil(), std::time::Instant::now()))
+}
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,47 @@ mod handler;
 mod model;
 mod schema;
 
+use crate::actor::{db::DbActor, ws::WsActor};
+
+use crate::handler::{get_ws, login, send_msg};
+use crate::model::AppState;
+use actix::prelude::*;
+use actix_web::{http::Method, server, App};
+use diesel::{r2d2::ConnectionManager, PgConnection};
+use dotenv::dotenv;
+
+use std::collections::HashMap;
+use std::env;
+
 
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set!");
+
+    let sys = actix::System::new("chat_system");
+
+    let manager = ConnectionManager::<PgConnection>::new(db_url);
+
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to build the Pool");
+
+    let db_addr: Addr<DbActor> = SyncArbiter::start(5, move || DbActor(pool.clone()));
+    let ws_addr: Addr<WsActor> = Arbiter::start(|_| WsActor(HashMap::new()));
+
+    server::new(move || {
+        App::with_state(AppState {
+            db: db_addr.clone(),
+            ws: ws_addr.clone(),
+        })
+        .resource("/login", |r| r.method(Method::POST).with(login))
+        .resource("/get_ws", |r| r.method(Method::GET).f(get_ws))
+        .resource("/send", |r| r.method(Method::POST).with(send_msg))
+    })
+    .bind("localhost:8080")
+    .unwrap()
+    .start();
+
+    let _ = sys.run();
 }
